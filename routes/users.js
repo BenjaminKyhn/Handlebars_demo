38fa5871b915f69721581e8a0d4923e8e9081953
Benjamin Kyhn <benkyhn@hotmail.com>
2020-09-23 11:18:06 +0200

    JSON object for later use

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let obj = {
+    "id": "1337"
+  };
+  res.send(obj);
 });
 
 module.exports = router;
