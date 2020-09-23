1d360abe1261c3e2414847052de72f4639670074
Benjamin Kyhn <benkyhn@hotmail.com>
2020-09-23 12:07:58 +0200

    En tabel fyldes med data fra JSON

--- routes/bookinger.js
+++ routes/bookinger.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.sendFile('/public/biler.json', { root: 'C:/Users/benky/Node/Handlebars' })
+  //res.sendFile('/public/biler.json', { root: 'C:/Users/benky/Node/Handlebars' })
+  res.render('bookinger', { title: 'Bookinger' });
 });
 
 module.exports = router;
