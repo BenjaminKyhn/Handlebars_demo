0b3b297753139ba8612cad72c76abf0abdd30813
Benjamin Kyhn <benkyhn@hotmail.com>
2020-09-23 12:21:25 +0200

    Flyttet en fil

--- routes/bookinger.js
+++ routes/bookinger.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.sendFile('/public/biler.json', { root: 'C:/Users/benky/Node/Handlebars' })
   res.render('bookinger', { title: 'Bookinger' });
 });
 
